e871122106b979051eae5cef657a5c49e2b4fded
Christian Ewert <CEwert@gmx.net>
2025-10-14 20:31:03 +0200

    Initial commit: migrate to NixOS flake setup

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Mischkas NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    # Falls du später Home Manager oder andere Inputs nutzt:
+    # home-manager.url = "github:nix-community/home-manager/release-25.05";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
