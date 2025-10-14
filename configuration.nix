041446f96ef746ac6b3473477c95a7b389c48efc
Christian Ewert <CEwert@gmx.net>
2025-10-14 22:04:01 +0200

    remove flatpak

--- configuration.nix
+++ configuration.nix
@@ -68,16 +68,7 @@
   services.printing.enable = true;
 
   # Enable Flatpak Support
-  services.flatpak.enable = true;
-
-  systemd.services.flatpak-repo = {
-    wantedBy = [ "multi-user.target" ];
-    path = [ pkgs.flatpak ];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-    '';
-  };
-
+  #services.flatpak.enable = true;
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
