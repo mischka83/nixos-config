0523dd506005ad5e5b6ce3c8ae4e06e6a5c14ea0
Christian Ewert <CEwert@gmx.net>
2026-01-14 21:58:56 +0100

    repair repo

--- modules/core/virtualisation.nix
+++ modules/core/virtualisation.nix
@@ -2,9 +2,11 @@
 
 {
   virtualisation = {
-    docker.enable = true;
-    libvirtd.enable = true;
+    docker.enable = false;
+    libvirtd.enable = false;
     virtualbox.host.enable = true;
+    virtualbox.host.enableKvm = true;
+    virtualbox.host.addNetworkInterface = false;
   };
   users.extraGroups.vboxusers.members = [ "mischka" ];
 
