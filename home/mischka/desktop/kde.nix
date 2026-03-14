796a29d8428137f944a6bb8c4e0e505a5df9d1b7
Christian Ewert <CEwert@gmx.net>
2026-03-14 14:25:53 +0100

    fix: nerdfonts zu nerd-fonts korrigieren

--- home/mischka/desktop/kde.nix
+++ home/mischka/desktop/kde.nix
@@ -2,12 +2,38 @@
 
 {
   # KDE Plasma Dark Theme Konfiguration
+  xdg.configFile."kdeglobals" = {
+    text = ''
+      [General]
+      ColorScheme=BreezeDark
+      font=Noto Sans,11,-1,5,50,0,0,0,0,0
+      menuFont=Noto Sans,11,-1,5,50,0,0,0,0,0
+      smallestReadableFont=Noto Sans,8,-1,5,50,0,0,0,0,0
+      toolBarFont=Noto Sans,11,-1,5,50,0,0,0,0,0
+
+      [Icons]
+      Theme=breeze-dark
+    '';
+  };
+
+  # Nerd Fonts - für bessere Icon Support
+  home.packages = with pkgs; [
+    # Beliebte Nerd Fonts
+    (nerd-fonts.override { fonts = [ "FiraCode" "JetBrainsMono" "Meslo" ]; })
+
+    # KDE Theme Pakete
+    kdePackages.breeze
+    kdePackages.breeze-icons
+  ];
+
+  # Qt Stil für KDE Apps
   qt = {
     enable = true;
     platformTheme.name = "kde";
     style.name = "breeze";
   };
 
+  # GTK Stil (für nicht-KDE Apps)
   gtk = {
     enable = true;
     theme = {
@@ -19,16 +45,6 @@
       package = pkgs.kdePackages.breeze-icons;
     };
   };
-
-  # Nerd Fonts Installation - konkrete Fonts
-  home.packages = with pkgs; [
-    # Nerd Fonts (einzeln, um Speicher zu sparen)
-    (pkgs.nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" "Meslo" ]; })
-
-    # KDE Plasma Theme packages
-    kdePackages.breeze
-    kdePackages.breeze-icons
-  ];
 
   # Session Variable für Terminal
   home.sessionVariables = {
