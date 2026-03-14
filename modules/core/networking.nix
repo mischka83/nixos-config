5b71d6474fd9a4649d8ca436706235e7a75fd8dd
Christian Ewert <CEwert@gmx.net>
2026-03-14 17:45:06 +0100

    feat: NetworkManager OpenConnect Plugin für Cisco AnyConnect VPN
    - networking.networkmanager.plugins: networkmanager-openconnect
    - environment.systemPackages: openconnect CLI Tool
    
    Erlaubt VPN-Verbindungen mit Cisco AnyConnect kompatiblem Modus

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -2,4 +2,14 @@
 
 {
   networking.networkmanager.enable = true;
+
+  # OpenConnect VPN Plugin für Cisco AnyConnect Kompatibilität
+  networking.networkmanager.plugins = with pkgs; [
+    networkmanager-openconnect
+  ];
+
+  # OpenConnect CLI Tool
+  environment.systemPackages = with pkgs; [
+    openconnect
+  ];
 }
