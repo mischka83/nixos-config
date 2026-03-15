2a2d3b323d1474a7e47bef9b6d9e8321fef33ce4
Christian Ewert <CEwert@gmx.net>
2026-03-15 10:38:12 +0100

    nix: enable store optimisation and gc

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -2,7 +2,18 @@
 
 {
   config = {
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    nix = {
+      settings = {
+        experimental-features = [ "nix-command" "flakes" ];
+        auto-optimise-store = true;
+      };
+
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 14d";
+      };
+    };
 
     nixpkgs.config.allowUnfree = true;
   };
