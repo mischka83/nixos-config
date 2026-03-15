37374d2f4ed8adb09225d309e9b67d97043a6cd6
Christian Ewert <CEwert@gmx.net>
2026-03-15 10:24:27 +0100

    home: fix plasma shortcuts

--- home/mischka/desktop/plasma/shortcuts.nix
+++ home/mischka/desktop/plasma/shortcuts.nix
@@ -5,22 +5,22 @@
   programs.plasma.krunner.shortcuts.launch = "Meta+Space";
   programs.plasma.krunner.shortcuts.runCommandOnClipboard = "Meta+Shift+V"; # optional
 
-  # Dolphin
-  # programs.plasma.dolphin.shortcuts.launch = "Meta+E";
+  # Dolphin (wie Windows-Explorer)
+  programs.plasma.shortcuts."services/org.kde.dolphin.desktop"._launch = "Meta+E";
 
   # KWin (Fensterverwaltung)
   programs.plasma.shortcuts.kwin = {
     "Switch to Desktop 1" = "Meta+1";
     "Switch to Desktop 2" = "Meta+2";
 
-    # Window Tiling (intern: QuickTileWindow...)
-    "QuickTileWindowLeft"  = "Meta+Left";
-    "QuickTileWindowRight" = "Meta+Right";
-    "QuickTileWindowUp"    = "Meta+Up";
-    "QuickTileWindowDown"  = "Meta+Down";
+    # Aktives Fenster an Bildschirmränder andocken
+    "Window Quick Tile Left" = "Meta+Left";
+    "Window Quick Tile Right" = "Meta+Right";
+    "Window Quick Tile Top" = "Meta+Up";
+    "Window Quick Tile Bottom" = "Meta+Down";
 
     # Optional: Fenster maximieren/minimieren
-    "Maximize"             = "Meta+M";
-    "Minimize"             = "Meta+N";
+    "Window Maximize" = "Meta+M";
+    "Window Minimize" = "Meta+N";
   };
 }
