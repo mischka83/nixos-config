9601da5d668498630cc839b4d91eb1803e5cb056
Christian Ewert <CEwert@gmx.net>
2026-03-15 10:42:31 +0100

    gaming: enhance steam setup

--- modules/optional/gaming/steam.nix
+++ modules/optional/gaming/steam.nix
@@ -1,5 +1,18 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  programs.steam.enable = true;
+  programs.gamemode.enable = true;
+
+  programs.steam = {
+    enable = true;
+    protontricks.enable = true;
+    extest.enable = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    mangohud
+  ];
 }
