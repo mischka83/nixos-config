4db728e8841fde1074b046d8c7cf56991517c05b
Christian Ewert <CEwert@gmx.net>
2026-03-16 18:56:25 +0100

    feat(sddm): configure chili theme and fix syntax, add jellyfin flatpak

--- home/mischka/programs/flatpak.nix
+++ home/mischka/programs/flatpak.nix
@@ -15,6 +15,7 @@
         "${pkgs.flatpak}/bin/flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo"
         "${pkgs.flatpak}/bin/flatpak --user install -y --noninteractive flathub net.devolutions.RDM"
         "${pkgs.flatpak}/bin/flatpak --user install -y --noninteractive flathub com.github.IsmaelMartinez.teams_for_linux"
+        "${pkgs.flatpak}/bin/flatpak --user install -y --noninteractive flathub org.jellyfin.JellyfinDesktop"
       ];
     };
 
--- modules/core/desktop/displaymanager.nix
+++ modules/core/desktop/displaymanager.nix
@@ -1,12 +1,30 @@
 { config, pkgs, ... }:
 
 {
-  services.displayManager.sddm = {
-    enable = true;
-    theme  = "breeze";
+  services.displayManager = {
+    sddm = {
+      enable = true;
 
-    # SDDM-Hintergrundbild systemweit setzen (gleicher NixOS-Wallpaper wie der Desktop).
-    settings.Theme.Background =
-      "${pkgs.nixos-artwork.wallpapers.nineish-dark-gray}/share/backgrounds/nixos/nix-wallpaper-nineish-dark-gray.png";
+      # Wayland-Backend fuer SDDM aktivieren (empfohlen fuer KDE Plasma/Wayland)
+      wayland.enable = true;
+
+      # Aktives SDDM-Theme (muss auch in extraPackages vorhanden sein)
+      # Alternativen: "breeze" (KDE-Standard), "sddm-astronaut-theme"
+      theme = "sddm-chili-theme";
+    };
+
+    # Automatischen Login deaktiviert lassen (sicherer)
+    # Zum Aktivieren: enable = true und user setzen
+    autoLogin = {
+      enable = false;
+      # user = "mischka";
+    };
+
+    # Theme-Pakete bereitstellen, damit SDDM sie finden kann.
+    # sddm-astronaut-theme ist als Reserve eingebunden, aktuell nicht aktiv.
+    extraPackages = with pkgs; [
+      sddm-chili-theme
+      sddm-astronaut-theme
+    ];
   };
 }
